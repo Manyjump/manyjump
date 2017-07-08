b70864dfc843ce18a67a2ccc4f0d363afdd7768f
Brett Beekley <brettbeekley@gmail.com>
2017-07-08 14:00:23 -0700

    merged background

--- client/components/Background.jsx
+++ client/components/Background.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const Background = (props) => {
   return (
     <div id='imgContainer'>
-      <img id='backgrImg1' src='client/resources/mountains-back.png' style={{ height:'400px', left:props.bkgr1PosX }}/>
-      <img id='backgrImg2' src='client/resources/mountains-back.png' style={{ height:'400px', left:props.bkgr2PosX }}/>
+      <img id='backgrImg1' src='assets/mountains-back.png' style={{ height:'400px', left:props.bkgr1PosX }}/>
+      <img id='backgrImg2' src='assets/mountains-back.png' style={{ height:'400px', left:props.bkgr2PosX }}/>
     </div>
   );
 }
