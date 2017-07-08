331bae5497e5c5c15da97912071d541c30fec64e
KseniaAK <ksenia.kozh@gmail.com>
2017-07-07 21:52:38 -0700

    integrated moving background into the App component

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import Character from './Character.jsx';
+import Background from './Background.jsx';
 
 function getInitialState() {
   return {
     jump: false,
-    lastJumpTime: 0
+    lastJumpTime: 0,
+    bkgr1PosX: '0px',
+    bkgr2PosX: '916px',
   };
 }
 
@@ -13,6 +16,7 @@ class App extends Component {
     super(props);
     this.handleKey = this.handleKey.bind(this);
     this.setFalse = this.setFalse.bind(this);
+    this.update = this.update.bind(this);
     this.state = getInitialState();
   }
 
@@ -33,19 +37,48 @@ class App extends Component {
     if (this.state.jump) this.setState({jump : false});
   }
 
+  update() {
+    console.log('in update');
+
+    this.setState((prevState) => {
+      const prevX1 = Number(prevState.bkgr1PosX.slice(0, -2));
+      const prevX2 = Number(prevState.bkgr2PosX.slice(0, -2));
+      let newX1;
+      let newX2;
+      if (prevX1 === -915) {
+        newX1 = '916px';
+        newX2 = prevX2 - 1 + 'px';
+      } else if (prevX2 === -915) {
+        newX1 = prevX1 - 1 + 'px';
+        newX2 = '916px';
+      } else {
+        newX1 = prevX1 - 1 + 'px';
+        newX2 = prevX2 - 1 + 'px';
+      }
+      return {
+        bkgr1PosX: newX1,
+        bkgr2PosX: newX2,
+      }
+    });
+  }
 
   componentDidMount() {
     // window.addEventListener('keyup',   this.handleKey.bind(this, false));
     window.addEventListener('keypress', this.handleKey.bind(this, true));
+    setInterval(this.update, 10);
   }
 
   render() {
 
     // document.body.onkeyup = this.handleKey;
     const style = this.state.jump ? ' jump' : '' ;
+    const { bkgr1PosX, bkgr2PosX } = this.state;
 
     return (
-      <Character jump={style} />
+      <div id='background'>
+        <Background bkgr1PosX={bkgr1PosX} bkgr2PosX={bkgr2PosX} />
+        <Character jump={style} />
+      </div>
     )
   }
 }
