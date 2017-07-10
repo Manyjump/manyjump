5447c9df031d8a5a156280c6bf902f2f8fa43044
OnuPete <pcr137@case.edu>
2017-07-10 10:55:00 -0700

    notifications merged

--- client/components/Notifications.jsx
+++ client/components/Notifications.jsx
@@ -5,16 +5,16 @@ const Notifications = (props) => {
   // Create jsx for each message
   // Apply styling based on the type of message
   const users = Object.keys(props.users).map((id) => {
-    
+
     const style = {
       color: 'pink',
       filter: `hue-rotate(${props.users[id].colorRotation}) saturate(200%) brightness(75%)`,
       fontWeight: 'bold'
     }
-    
+
     return <li key={id}><span style={style}>{props.users[id].name}</span> has joined!</li>
   });
-  
+
   return (
     <ul className='notifications'>
       {users}
